e608760f310683c61b02c91b8173f2e2c9bce22b
lsioctl <laurent.steff@gmail.com>
2023-12-06 10:54:01 -0400

    simple version of get

---
+++ async-get-nofutures/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::{prelude::*, BufReader};
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let server = "127.0.0.1:8080";
+
+    let mut stream = TcpStream::connect(server)?;
+
+    // Note: it seems that the Write trait
+    // is included with std::io::prelude::*
+    stream.write(b"GET / HTTP/1.0\n\n")?;
+
+    // I will use BufReader instead
+    // let mut received = [0; 128];
+    // stream.read(&mut received)?;
+    // TODO: could not use ? because of mismatch of error types
+    // could be interesting to dig ?
+    // FromUtf8Error, and 
+    // println!("{}", String::from_utf8(received.to_vec()).unwrap());
+
+    let buffer_reader = BufReader::new(&mut stream);
+
+    buffer_reader
+    .lines()
+    .map(|line| line.unwrap())
+    .for_each(|line| { println!("{}", line)});
+
+
+    Ok(())
+}
--- reactor/README.md
+++ reactor/README.md
@@ -1,3 +1,19 @@
+# Working project to better understand async in Rust
+
 Inspired from:
 
 https://www.zupzup.org/epoll-with-rust/index.html
+
+Note: reactor seems to be a bad name, I stop working
+on it for now, and prefer a bit higher level with MIO
+as my interest is how the Futures in Rust are coupled
+with event loops (link between an "epoll" (epoll/kqueue/iocp)
+and poll of Rust futures).
+
+If I want to dig more, I have to be wary of what are the best
+practices with sockets, e.g:
+
+https://stackoverflow.com/questions/10187347/async-connect-and-disconnect-with-epoll-linux
+
+and for storage (as it seems a thread pool dedicated to blocking instances is the
+way of doing it now) (Question: and what about io_uring ?)
